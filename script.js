af77a69a493394cd454eac79d2b3d16bf62b0dae
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-02-20 16:49:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function menuShow(){
     }
 }
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slideshow-slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[slideIndex - 1].classList.add("active");
+}
+
+let autoSlide = setInterval(() => {
+    plusSlides(1);
+  }, 4000);
+
